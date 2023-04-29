f0d7f7372d7bd332432e08fdc754f7f4238c4185
자신의 <wnsgh5049@naver.com>
2023-04-29 21:08:54 +0900

    23.04.29.(토) cpp - 배열 및 cout-string

---
+++ PlatformCPPApp/TBCPP/TBCPP/TBCPP/6.2_array_basic_2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+void doSomething(int students_scores[20]) // 컴파일러는 매개변수를 포인터로 처리한다.
+{
+    cout << (int)&students_scores << endl;  //1547303540
+    // 포인터 변수 첫번째 주소값을 저장하는 포인터 변수이다.
+    cout << (int)&students_scores[0] << endl;//1547303520
+    cout << students_scores[0] << endl;     //1 
+    cout << students_scores[1] << endl;     //2
+    cout << "Size in doSomething" << sizeof(students_scores) << endl;
+}
+
+
+int main()
+{
+    const int num_students = 20;
+
+    int students_scores[num_students] = {1,2,3,4,5,};
+    cout << (int)students_scores << endl;       //1547303520
+    cout << (int)&students_scores << endl;      //1547303520
+    cout << (int)&students_scores[0] << endl;   //1547303520
+    cout << (int)&students_scores[1] << endl;   //1547303524
+    cout << (int)&students_scores[2] << endl;   //1547303528
+    cout << (int)&students_scores[3] << endl;   //1547303532
+    cout << (int)&students_scores[4] << endl;   //1547303536
+    cout << "Size in main" << sizeof(students_scores) << endl;
+
+    cout << "doSomething func start" << endl;
+
+
+    doSomething(students_scores);
+}
---
+++ PlatformCPPApp/TBCPP/TBCPP/enc_temp_folder/b666abb16199104bbac8c475ccbc85d3/6.10.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+const char* getName()
+{
+	return "jackjack";
+}
+
+int main()
+{
+	//char name[] = "Jack Jack"; // Jack Jack
+
+	// char *name = "Jack Jack"; // Error
+
+	/*
+	const char *name = "Jack Jack";
+	const char *name2 = "Jack Jack";
+	//const char* name = getName();	//위와 상동
+	//const char* name2 = getName();//위와 상동
+	// 문자열 포인터 앞에 const을 붙이면 문자열을 기호적 상수로 사용할 수 있음
+
+	//char name[] = "Jack Jack";
+
+	cout << (uintptr_t)name << endl;	//3250992
+	cout << (uintptr_t)name2 << endl;	//3250992
+	*/
+
+	int int_arr[5] = { 1,2,3,4,5 };
+	char char_arr[] = "Hello, World";
+	const char* name = "jackjack";
+
+	cout << int_arr << endl;	// 00B3FA94
+	cout << char_arr << endl;	// Hello, World
+	cout << name << endl;		// jackjack
+	// 주의: cout은 문자열 포인터는 특별히 null character까지의 값을 반환한다.()
+
+	char c = 'Q';
+	cout << &c << endl; //Q儆儆儆儆0썌 
+	// cout은 문자열 포인터를 값을 읽어오기 때문에 null character까지의 값을 읽어와 Q 뒤에 값이 출력된다.
+
+
+
+	return 0;
+}
