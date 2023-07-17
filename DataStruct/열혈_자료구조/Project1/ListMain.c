d0bc18c077188d0e53f309e0292430b64a8a68b3
jun-ho-Kim <wnsgh5049@naver.com>
2023-07-17 19:27:21 +0900

    23.07.17.(월) 개인 공부 - 열혈 자료구조 3.1 Array 예제

---
+++ DataStruct/열혈_자료구조/Project1/ListMain.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include "ArrayList.h"
+
+int main()
+{
+	List list;
+	int data;
+	ListInit(&list);
+
+	LInsert(&list, 11);
+	LInsert(&list, 11);
+	LInsert(&list, 22);
+	LInsert(&list, 22);
+	LInsert(&list, 33);
+
+	// 저장된 데이터의 전체 출력
+	printf("현재 데이터 수: %d \n", LCount(&list));
+
+	if (LFirst(&list, &data))
+	{
+		printf("%d ", data);
+
+		while (LNext(&list, &data))
+			printf("%d ", data);
+	}
+
+	printf("\n\n");
+
+	if (LFirst(&list, &data))
+	{
+		if (data == 22)
+			LRemove(&list);
+
+		while (LNext(&list, &data))
+		{
+			if (data == 22)
+				LRemove(&list);
+		}
+	}
+
+	// 삭제 후 남은 데이터 전체 출력
+	printf("현재 데이터의 수: %d \n", LCount(&list));
+
+	if (LFirst(&list, &data))
+	{
+		printf("%d ", data);
+
+		while (LNext(&list, &data))
+			printf("%d ", data);
+	}
+
+	printf("\n\n");
+	return 0;
+}
