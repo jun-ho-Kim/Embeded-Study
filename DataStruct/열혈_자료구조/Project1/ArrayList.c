c2ce201eb03935acc163f898c0fe83c211ae8a71
jun-ho-Kim <wnsgh5049@naver.com>
2023-07-18 19:41:27 +0900

    23.07.18.(화) 개인 공부 - 열혈 자료구조 연결리스트 3.1 PointList 예제

--- DataStruct/열혈_자료구조/Project1/ArrayList.c
+++ DataStruct/열혈_자료구조/Project1/ArrayList.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include "ArrayList.h"
 
+//plist init 초기화
 void ListInit(List* plist)
 {
 	(plist->numOfData) = 0;
 	(plist->curPosition) = -1;
 } 
 
+//plist arr 길이를 추가해 새로 추가된 길이에 data 인자 값 추가
 void LInsert(List* plist, LData data)
 {
 	if (plist->numOfData >= LIST_LEN)
@@ -19,17 +21,20 @@ void LInsert(List* plist, LData data)
 	(plist->numOfData)++;
 }
 
+//현재 위치(curPosition)에 위치한 데이터를 pData 주소 파라미터에 저장
 int LFirst(List* plist, LData* pdata)
 {
 	if (plist->numOfData == 0)
 		return FALSE;
 
 	(plist->curPosition) = 0;
+	printf("LFirst plist->arr[0] is %d\n", plist->arr[0]);
 	*pdata = plist->arr[0];
 
 	return TRUE;
 }
 
+//plist의 다음 위치의 데이터 값을 pdata 주소 파라미터로 저장
 int LNext(List* plist, LData* pdata)
 {
 	if (plist->curPosition >= (plist->numOfData) - 1)
@@ -40,6 +45,8 @@ int LNext(List* plist, LData* pdata)
 	return TRUE;
 }
 
+
+//LRemove - plist의 현재 위치의 데이터 부터 끝까지 삭제
 LData LRemove(List* plist)
 {
 	int rpos = plist->curPosition;
