0385c05eb8a9682bb656eea01051545c1c50b821
jun-ho-Kim <wnsgh5049@naver.com>
2023-07-27 19:31:38 +0900

    23.07.27.(목) - 개인공부-열혈 자료구조 -  4.3 단원 예제 DLinkedListSortMain.c, 파일 분석 및 자료구조 그림 ppt로 그려보기

--- DataStruct/열혈_자료구조/Project1/Q4.2_DLinkedRead.c
+++ DataStruct/열혈_자료구조/Project1/Q4.2_DLinkedRead.c
@@ -3,7 +3,7 @@
 
 // 더미 노드를 추가하고, 그에 따른 코드의 변화를 직접 확인하기로 하겠다.
 // 이 예제는 리스트이 끝에다가 노드를 추가하는 방식으로 head와 tail이 모두 필요하다.
-// Q4.1_LinkedRead.c 코드와 동일
+
 typedef struct __node
 {
 	int data;
@@ -20,6 +20,9 @@ int main(void)
 	Node* newNode = NULL;
 	int readData;
 
+	head = (Node*)malloc(sizeof(Node));
+	tail = head;
+
 	//데이터를 입력 받는 과정 ////
 	while (1)
 	{
@@ -33,15 +36,16 @@ int main(void)
 		newNode->data = readData;
 		newNode->next = NULL;
 
-		// 내가 Q4.1에서 수정한 코든
-		if (head == NULL)
-		{
-			dummy->data = "DMY";
-			dummy->next = newNode;
-			head->next = dummy;
-		}
-		else
-			tail->next = newNode;
+		// 내가 Q4.1에서 수정한 코드
+		//if (head == NULL)
+		//{
+		//	dummy->data = "DMY";
+		//	dummy->next = newNode;
+		//	head->next = dummy;
+		//}
+		//else
+
+		tail->next = newNode;
 
 		tail = newNode;
 	}
@@ -49,14 +53,14 @@ int main(void)
 
 	//입력 받은 데이터의 출력과정 ////
 	printf("입력 받은 데이터의 전체 출력! \n");
-	if (head == NULL)
+	if (head == tail)
 	{
 		printf("저장된 자연수가 존재하지 않습니다. \n");
 	}
 	else
 	{
 		cur = head;
-		printf("%d ", cur->data);
+		//printf("%d ", cur->data);
 
 		while (cur->next != NULL)
 		{
@@ -68,18 +72,17 @@ int main(void)
 	printf("\n\n");
 
 	//메모리의 해제과정 ////
-	if (head == NULL)
+	if (head == tail)
 	{
 		return 0;
-	} 
+	}
 	else
 	{
 		Node* delNode = head;
 		Node* delNextNode = head->next;
 
-		printf("%d을(를) 삭제합니다. \n", head->data);
-
-		free(delNode);
+		//printf("%d을(를) 삭제합니다. \n", head->data);
+		//free(delNode);
 
 		while (delNextNode != NULL)
 		{
@@ -90,9 +93,4 @@ int main(void)
 			free(delNode);
 		}
 	}
-
-	
-
-	
-
 }
