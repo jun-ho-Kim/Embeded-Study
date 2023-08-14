f337712f89fb3f1853d4e414f352529d5ee7184f
jun-ho-Kim <wnsgh5049@naver.com>
2023-08-14 16:31:51 +0900

    23.08.14.(월) 인텔 AI 팀 프로젝트 - 파일 업로드 frontend, backend init 및 초안

---
+++ intel_teamProject_web/server/app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+
+const app = express();
+const cors = require('cors');
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const router = express.Router();
+app.use(cors());
+app.use("/static", express.static(path.join(__dirname, "static")));
+
+const port = 4000;
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) 
+    {
+        cb(null, 'uploads/')
+    },
+    filename:function (req, file, cb)
+    {
+        cb(null, file.fieldname + '-', Date.now())
+    }
+});
+
+const uploads = multer({storage})
+
+app.get('/', (req, res) =>{
+    res.send('Hello World!');
+});
+
+
+app.post('/file', uploads.single('img'), (req, res,next) =>{
+
+    console.log("server uploads is ", uploads)
+
+    console.log("req is ", req)
+/*
+    res.status.send({
+        messege:"OK",
+        fileInfo: req.file
+    })
+    
+
+    */
+   //console.log(req.file);
+   //res.json({ url : `/img/${req.file.filename}`});
+    res.send('Hello World!');
+})
+
+app.listen(port, () => {
+    console.log('서버가 실행됩니다. ')
+})
