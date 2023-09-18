1808759220e5fc0f1d6af9d725b9d6d3f7429c80
jun-ho-Kim <wnsgh5049@naver.com>
2023-09-18 18:58:37 +0900

    23.09.18.(월) 센터 수업 - 리눅스 디바이스 드라이버(1일차 module_init+led 제어 함수)

---
+++ device driver/p106_led/p106_led.c
@@ -0,0 +1,75 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/gpio.h>
+#define OFF 0
+#define ON 1
+#define gpioName(a,b) #a#b     //"led""0" == "led0"
+#define GPIOLEDCNT 8
+
+int gpioLed[GPIOLEDCNT] = {6,7,8,9,10,11,12,13};
+
+int gpioLedInit(void);
+void gpioLedSet(long);
+void gpioLedFree(void);
+
+static int hello_init(void)
+{
+	printk("Hello World\n");
+	gpioLedInit();
+	gpioLedSet(0xff);
+	return 0;
+}
+
+static void hello_exit(void)
+{
+	printk("Goodbye, world\n");
+	gpioLedSet(0x00);
+	gpioLedFree();
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_LICENSE("Dual BSD/GPL");
+
+
+int	gpioLedInit(void)
+{
+	int i;
+	int ret = 0;
+	for(i=0;i<GPIOLEDCNT;i++)
+	{
+		ret = gpio_request(gpioLed[i], gpioName(led,i));
+		if(ret < 0) {
+			printk("Failed Request gpio%d error\n", 6);
+			return ret;
+		}
+	}
+	for(i=0;i<GPIOLEDCNT;i++)
+	{
+		ret = gpio_direction_output(gpioLed[i], OFF);
+		if(ret < 0) {
+			printk("Failed direction_output gpio%d error\n", 6);
+       	 return ret;
+		}
+	}
+	return ret;
+}
+
+void gpioLedSet(long val) 
+{
+	int i;
+	for(i=0;i<GPIOLEDCNT;i++)
+	{
+		gpio_set_value(gpioLed[i], (val>>i) & 0x01);
+	}
+}
+void gpioLedFree(void)
+{
+	int i;
+	for(i=0;i<GPIOLEDCNT;i++)
+	{
+		gpio_free(gpioLed[i]);
+	}
+}
